0c6e43272428df921d1db17a1b283dda4881beca
yuzx09 <yu@zhoux.me>
2025-03-20 12:22:55 +0900

    add dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
-//const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports =  () => ({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: './src/index.js',
   target: 'web',
   mode: 'development',
   output: {
-    //path: path.resolve(__dirname, 'dist'),
-    path: '/home/ivo/isomorphic-ws/example/webpack/dist/',
-    filename: 'main.js',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-});
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    hot: true,
+    open: true,
+    port: 8080,
+    historyApiFallback: true,
+  },
+};
